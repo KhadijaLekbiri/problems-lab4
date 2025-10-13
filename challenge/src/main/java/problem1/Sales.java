c93e18f704fd3a49470f24e0a8461e4db6c3fe60
Khadija <lekbirikhadija3@gmail.com>
2025-10-13 11:31:54 +0100

    exo1

--- challenge/src/main/java/problem1/Sales.java
+++ challenge/src/main/java/problem1/Sales.java
@@ -4,23 +4,47 @@ public class Sales
 {
     public static void main(String[] args)
     {
-        final int SALESPEOPLE = 5;
-        int[] sales = new int[SALESPEOPLE];
-        int sum;
+        int sum, max, min, val, num = 0;
         Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number of sales people :");
+
+        final int SALESPEOPLE = scan.nextInt();
+        int[] sales = new int[SALESPEOPLE];
+
         for (int i=0; i<sales.length; i++)
         {
-            System.out.print("Enter sales for salesperson " + i + ": ");
+            System.out.print("Enter sales for salesperson " + (i+1) + ": ");
             sales[i] = scan.nextInt();
         }
         System.out.println("\nSalesperson Sales");
         System.out.println("--------------------");
-        sum = 0;
+        sum = 0;max = sales[0];min = sales[0];
         for (int i=0; i<sales.length; i++)
         {
-            System.out.println(" " + i + " " + sales[i]);
+            if (max < sales[i]){
+                max = sales[i];
+            }
+            if (min > sales[i]){
+                min = sales[i];
+            }
+            System.out.println(" " + (i+1) + " " + sales[i]);
             sum += sales[i];
         }
         System.out.println("\nTotal sales: " + sum);
+        System.out.println("Average sale: " + sum/SALESPEOPLE);
+        System.out.println("The maximum sale: " + max);
+        System.out.println("The minimum sale: " + min);
+
+        System.out.print("\nEnter a value :");
+        val = scan.nextInt();
+
+        for (int i=0; i<sales.length; i++) {
+            if (sales[i] >= val){
+                num++;
+                System.out.println(" " + (i+1) + " " + sales[i]);
+            }
+        }
+        System.out.println("The number of salespeople whose sales exceeded the value "+val+" is :" + num);
     }
 }
